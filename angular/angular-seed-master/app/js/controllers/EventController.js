6681753016ac6ea638cdbcd35969eb240d8a88e8
Muhammad Usman <muhammad@usmanity.com>
2014-05-27 21:10:14 -0700

    strings file added

--- angular/angular-seed-master/app/js/controllers/EventController.js
+++ angular/angular-seed-master/app/js/controllers/EventController.js
@@ -1,24 +1,7 @@
 eventsApp.controller('EventController',
-    function EventController($scope){
+    function EventController($scope, eventData){
         $scope.sortorder = 'name';
-        $scope.event = {
-            name: 'Angular',
-            date: '05/01/2013',
-            location: {
-                address: "260 Coleridge Street",
-                city: "San Francisco"
-            },
-            sessions: [
-                {
-                name: "google",
-                upvoteCount: 0
-                },
-                {
-                name: "yahoo",
-                upvoteCount: 0
-                }
-            ]
-        }
+        $scope.event = eventData.event;
         $scope.upVoteSession = function(session){
             session.upvoteCount++;
         }
---
+++ ruby/strings.rb
@@ -0,0 +1,9 @@
+long_string = <<EOF
+  Here is a long string
+  With many paragraphs
+EOF
+
+puts long_string
+
+string = "a longer task is different than this"
+puts string.slice(2, 6)
