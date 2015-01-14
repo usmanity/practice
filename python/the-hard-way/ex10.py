e9cc1979d6d80d0010a5e9c5b09617f681b590bc
Muhammad Usman <muhammad@usmanity.com>
2015-01-14 11:59:29 -0800

    some exercises added

---
+++ python/the-hard-way/ex10.py
@@ -0,0 +1,9 @@
+print "I'm 5'7\" tall"
+
+tabbed = "\tI'm tabbed in"
+
+print tabbed
+
+tabbed_single_quotes = '\tI\'m tabbed in'
+
+print tabbed_single_quotes
---
+++ python/the-hard-way/ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+location = raw_input("Where are you from? ")
+
+print "So you're %s tall at %s years old from %s" % (height, age, location)
