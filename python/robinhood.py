789388dc458e2a42d8309b1bc65ec0d0234c7ae1
Muhammad Usman <muhammad@usmanity.com>
2015-11-22 12:41:21 -0800

    removing this unused file

--- python/robinhood.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-page = requests.get('https://www.robinhood.com/waitlist/93d0a061-4b50-46c8-89e2-69fefe605760/')
-soup = BeautifulSoup(page.text)
-
-print str(soup.h4.strong.get_text())
-
