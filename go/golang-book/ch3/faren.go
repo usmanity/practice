4a54689bf0d55587295b14eb246ee514db78c3ce
Muhammad Usman <muhammad@usmanity.com>
2016-10-19 23:37:08 -0700

    A LOT of clean up now, didn't want these old folders to be hanging around

--- go/golang-book/ch3/faren.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-  var farenheit float32 = 100.0
-  var celsius float32 = (farenheit - 32) * 5/9
-  fmt.Println("Celsius is", celsius)
-}
--- go/golang-book/ch3/feet.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-  var meter float32 = 0.3048
-  var foot float32 = 60 * meter
-  fmt.Println(foot)
-}
