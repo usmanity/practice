069b320feca09f5e3cf1a1b49d98518be9ff979e
Muhammad Usman <muhammad@usmanity.com>
2017-09-13 23:40:46 -0700

    add a translate file

---
+++ python/real/translate.py
@@ -0,0 +1,8 @@
+original = input("Enter some text: ")
+
+replaced = original.replace('a', '4').replace('b', '8')
+replaced = replaced.replace('e', '3').replace('l', '1')
+replaced = replaced.replace('o', '0').replace('s', '5')
+replaced = replaced.replace('t', '7')
+
+print(replaced)
