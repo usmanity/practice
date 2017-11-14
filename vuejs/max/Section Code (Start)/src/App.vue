ea5d60ae8a14faf286efbdc3e75907c7941512bb
Muhammad Usman <muhammad@usmanity.com>
2017-11-14 08:55:59 -0800

    adding some starting code for a practice vue js project

---
+++ vuejs/max/Section Code (Start)/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <app-servers></app-servers>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Good'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
