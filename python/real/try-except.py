52bfefa0389dd33e320062caa4756edea2ef6379
Muhammad Usman <muhammad@usmanity.com>
2018-03-06 22:05:25 -0800

    adding a way to try try except

---
+++ python/real/try-except.py
@@ -0,0 +1,12 @@
+print("360 is a small but very divisible number, try dividing it a small number")
+
+try:
+    number = int(input('enter a number: '))
+    if (360 / number) % 2 == 0:
+        print("360 is divisible by {}!".format(number))
+    else:
+        print("try another number.")
+except ValueError:
+    print('Please input a number.')
+except ZeroDivisionError:
+    print('You can\'t divide using zero.')
