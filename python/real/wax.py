03b6f3f818004cd15ab043d8710c8b008e3680d2
Muhammad Usman <muhammad@usmanity.com>
2018-03-29 10:20:20 -0700

    add a wax file

--- python/real/wax.py
+++ python/real/wax.py
@@ -1,1 +1,58 @@
-nouns = ["fossil", "horse", "aardvark"]
+import random
+
+nouns = [
+    "fossil", 
+    "horse", 
+    "aardvark",
+    "judge",
+    "chef",
+    "mango",
+    "extrovert",
+    "gorilla"
+]
+
+verbs = [
+    "kicks",
+    "jingles",
+    "bounces",
+    "slurps",
+    "meows",
+    "explodes",
+    "curdles"
+]
+
+adjectives = [
+    "furry",
+    "balding",
+    "incredulous",
+    "fragrant",
+    "exuberant",
+    "glistening"
+]
+
+prepositions = [
+    "against",
+    "after",
+    "into",
+    "beneath",
+    "upon",
+    "for",
+    "in",
+    "like",
+    "over",
+    "within"
+]
+
+adverbs = [
+    "curiously",
+    "extravagantly",
+    "tantalizingly",
+    "furiously",
+    "sensously"
+]
+
+print(
+    """
+        The %s %s
+    """
+% (random.choice(nouns), random.choice(verbs)))
