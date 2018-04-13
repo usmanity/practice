00a73e6c4c7ac8a44fdacad1ef6279b3534d6cdf
Muhammad Usman <muhammad@usmanity.com>
2018-04-12 22:55:13 -0700

    curlys gold

--- javascript/log.js
+++ javascript/log.js
@@ -1,13 +1,13 @@
 function logIterator(n) {
   var startTime = Date.now();
-  for (var i = 0; i <= n; ++i) {
-    setTimeout(function() {
+  for (var i = 0; i < n; i++) {
+    setTimeout(function(startTime, i) {
       var currentTime = Date.now();
       var deltaTime = currentTime - startTime;
       var roundedTime = Math.round( deltaTime / 1000 );
 
-      console.log( i + " after " + roundedTime + "s");
-    }, i * 1000);
+      console.log( Math.round((currentTime - startTime) / 1000) + " after " + roundedTime + "s");
+    }.bind(this, startTime, i), i * 1000);
   }
 };
 
