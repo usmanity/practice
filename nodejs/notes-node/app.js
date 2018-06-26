7f4e2830e00109c3468a427a4ce1ae5c39f520d9
Muhammad Usman <muhammad@usmanity.com>
2018-06-26 10:11:37 -0700

    adding notes example app setup

--- nodejs/notes-node/app.js
+++ nodejs/notes-node/app.js
@@ -1,14 +1,22 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// var res = notes.addNote();
-// console.log(res);
+const command = process.argv[2];
 
-var sum = notes.add(9, -2);
-console.log(sum);
+console.log(`command ${command}`);
 
-// const user = os.userInfo();
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if (command === 'add') {
+    console.log('adding using notes.js');
+} else if (command === 'list') {
+    console.log('listing notes');
+} else if (command === 'remove') {
+    console.log('removing notes');
+} else if (command === 'read') {
+    console.log('opening up a note for you');
+} else {
+    console.log(command);
+}
--- nodejs/notes-node/notes.js
+++ nodejs/notes-node/notes.js
@@ -4,8 +4,3 @@ module.exports.addNote = () => {
     console.log('Adding note');
     return 'new note';
 };
-
-module.exports.add = (a, b) => {
-    console.log(`Adding ${a} and ${b}`);
-    return a + b;
-}
