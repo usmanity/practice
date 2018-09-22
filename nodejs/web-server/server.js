c3d5638dae7804a9470f167a721b38746de94871
Muhammad Usman <muhammad@usmanity.com>
2018-09-22 10:07:06 -0700

    more setup for example web server

--- nodejs/web-server/server.js
+++ nodejs/web-server/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
+
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Error while logging');
+        }
+    });
+    // res.render('down', {
+    //     title: 'Maintenance mode',
+    //     message: `We'll be back soon!`
+    // });
+});
 
 app.get('/', (req, res) => {
     res.render('home', {
@@ -28,6 +46,6 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log(`Server is up on port 3000`);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
