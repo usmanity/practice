494824e1ff650ca1a11ba8b2b5a263f4083e4d1a
Muhammad Usman <muhammad@usmanity.com>
2018-09-27 16:33:06 -0700

    add more test

--- nodejs/node-tests/utils/utils.js
+++ nodejs/node-tests/utils/utils.js
@@ -1,1 +1,8 @@
 module.exports.add = (a, b) => a + b;
+module.exports.square = (x) => x * x;
+module.exports.setName = (user, name) => {
+    var names = name.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+}
--- nodejs/node-tests/utils/utils.test.js
+++ nodejs/node-tests/utils/utils.test.js
@@ -1,8 +1,31 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toEqual(44);
+});
+
+it('should square a number', () => {
+    var res = utils.square(4);
+    expect(res).toEqual(16);
+});
+
+it('should expect some values', () => {
+    expect(50).not.toEqual(51);
+    expect({
+        name: 'Jester'
+    }).toEqual({
+        name: 'Jester'
+    });
+    expect([2,3,4]).toEqual(expect.arrayContaining([3,4,2]));
+});
+
+it('should a user with name Baghat Singh to be set in a user object', () => {
+    var emptyUser = {};
+    var user = utils.setName(emptyUser, 'Baghat Singh');
+    expect(user).toEqual({
+        firstName: 'Baghat',
+        lastName: 'Singh'
+    });
 });
