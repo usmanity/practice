fece41f67b25e01b87ec56a25bbe70759229c59a
Muhammad Usman <muhammad@squareup.com>
2019-10-02 14:39:06 -0700

    removing php folder

--- php/laracasts/hello.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$greeting = htmlspecialchars($_GET['greeting']);
-$name = htmlspecialchars($_GET['name']);
-
-require("hello.view.php");
--- php/so-docs/readme.md
+++
@@ -1,1 +0,0 @@
-This folder is for learning from Stack Overflow docs!
